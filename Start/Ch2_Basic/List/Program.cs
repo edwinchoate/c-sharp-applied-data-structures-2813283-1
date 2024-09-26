e135b450b4c4e279063d2d207e472d920e35366a
Edwin Choate <edwin.choate@pm.me>
2024-09-26 16:54:02 -0400

    2.1 Basic List operations, 2.2 Searching List content

--- Start/Ch2_Basic/List/Program.cs
+++ Start/Ch2_Basic/List/Program.cs
@@ -12,46 +12,54 @@ namespace ListExample
                                 "George Carlin", "Lenny Bruce",
                                 "Eddie Murphy", "Louie Anderson"};
             // TODO: Create the list
-
+            List<string> list = new List<string>(10);  // 10 is Capacity
 
             // TODO: Add some elements to the list from an existing array
-
+            list.AddRange(comedians);
 
             // TODO: Add individual items
-
+            list.Add("Steve Martin");
 
             // TODO: Examine the Count and Capacity
-
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Capacity);
 
             // TODO: Enumerate the items in the list with a foreach loop
-
+            PrintList(list);
 
             // TODO: Access any item by its index
-
+            Console.WriteLine(list[5]);
 
             // TODO: Remove items from the list
+            list.RemoveAt(5);
 
+            Console.WriteLine();
 
             // TODO: Sort the list content
-
+            list.Sort();
+            PrintList(list);
 
             // ** Searching List Content **/
 
-            // TODO: Determine if a list contains a given item
+            Console.WriteLine();
 
+            // TODO: Determine if a list contains a given item
+            Console.WriteLine("Is Steve Martin in the list? {0}", list.Contains("Steve Martin"));
 
             // TODO: The Exists function gives us a more customizable way to search
             // by using a predicate function to examine each element in the list
-
+            Console.WriteLine("Is there a comedian whose name starts with 'G'? {0}", list.Exists(x => x.StartsWith("G")));
 
             // TODO: Use the Find function to search through items
-
+            Console.WriteLine("A comedian whose name starts with 'R' {0}", list.Find(x => x.StartsWith("R")));
 
             // TODO: The FindAll function can find multiple items
-
+            Console.WriteLine("Comedians whose names start with 'L':");
+            List<string> l = list.FindAll(x => x.StartsWith("L"));
+            PrintList(l);
 
             // TODO: Use TrueForAll to see if a given condition is true for all elements
-
+            Console.WriteLine(list.TrueForAll(x => x.Length > 3));
         }
 
         static void PrintList(List<string> theList)
--- notes.md
+++ notes.md
@@ -38,3 +38,73 @@ Fast adding/removing of data
 
 Doesn't require contiguous memory for storage
 * LinkedList
+
+## Ch. 2 Basic Data Structures 
+
+### Lists
+
+_List_ - a strongly-typed collection of objects that can be accessed by a zero-based index and is dynamically resized as needed based on what items are added or removed. 
+
+`Systems.Collections.Generic`
+
+Adding items to a list
+
+```C#
+myList.Add(5);
+
+myList.AddRange(new int[] {3, 4, 5, 6});  // Adding multiple items at once
+```
+
+Get the number of items in the list
+
+```C#
+int n = list.Count;
+```
+
+Remove items from the list
+
+```C#
+list.RemoveAt(3);  // remove what's stored at index=3
+
+stringList.Remove("hello");  // finds the value "hello" in the list and removes it
+```
+
+Sort a list
+
+```C#
+list.Sort();
+```
+
+Seeing if a specific value is in the list
+
+```C#
+list.Contains("hello"); // checks for an exact match
+```
+
+Checking the list based on some criteria
+
+```C#
+bool hasLargeNum = list.Exists(x => x > 500);  // sees if theres any # in the list > 500
+```
+
+Finds the first item found in the list that matches some criteria 
+
+```C#
+string match = list.Find(x => x.Length > 10);
+// match is set to the first string of length 10+ it finds 
+```
+
+Find multiple items in the list based on some criteria 
+
+```C#
+List<string> matches = list.FindAll(x => x.Length > 10);
+// matches returns all the strings in the original ist of length 10+
+```
+
+Check to see if every value in the list meets some criteria 
+
+```C#
+bool result = list.TrueForAll(x => x.Length > 10);
+// True if every string in the List<string> has a length >= 10
+```
+
